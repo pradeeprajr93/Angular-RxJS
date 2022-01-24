3c709081a2b261b704e0c50af51991e2c9d1e770
pradeeprajr93 <pradeeprajr93@gmail.com>
2022-01-24 12:14:16 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'pm-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   pageTitle = 'Acme Product Management';
+
+  obs$ = new Observable((observer) => {
+    observer.next('Hey');
+    observer.next('Hello!');
+    observer.complete();
+  });
+
+  ngOnInit() {
+    this.obs$.subscribe(console.log);
+  }
 }
